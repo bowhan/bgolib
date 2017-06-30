48ad1d4292d99fa3593df3ef42f8d2412559cc41
bowhan <bowhan@icloud.com>
2017-06-30 08:49:14 -0400

    remove non-standard fmt

--- blast/alignment.go
+++ blast/alignment.go
@@ -17,7 +17,6 @@ type Alignment struct {
 	Send     int
 	Evalue   float64
 	Bitscore float64
-	Nreads   int
 	Strand   byte
 }
 
@@ -36,7 +35,6 @@ func NewAlignment(nreads string, args ... string) *Alignment {
 	aln.Send, _ = strconv.Atoi(args[9])
 	aln.Evalue, _ = strconv.ParseFloat(args[10], 64)
 	aln.Bitscore, _ = strconv.ParseFloat(args[11], 64)
-	aln.Nreads, _ = strconv.Atoi(nreads)
 	if aln.Sstart < aln.Send {
 		aln.Strand = '+'
 	} else {
