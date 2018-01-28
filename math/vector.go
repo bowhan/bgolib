f9bf2c10964b21c347643722f6365653c6c1fa5a
bowhan <bowhan@icloud.com>
2018-01-28 14:07:15 -0500

    rename to please golint

--- math/vector.go
+++ math/vector.go
@@ -17,10 +17,10 @@
 
 package math
 
-// An alias for an integer slice
+// Vector An alias for an integer slice
 type Vector []int
 
-// Inner product of two vectors
+// InnerProduct Inner product of two vectors
 func (a Vector) InnerProduct(b Vector) Vector {
 	if len(a) != len(b) {
 		return nil
